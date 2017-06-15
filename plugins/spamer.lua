5e6f2cae6735cee79bbc24b0fd8904fc80699a65
errorscode <altair1379@gmail.com>
2017-06-15 10:19:43 +0430

    Create spamer.lua

---
+++ plugins/spamer.lua
@@ -0,0 +1,66 @@
+local triggers = {
+"^spam$",
+  "^fuck$",
+  }
+  
+local action = function(msg, matches)
+
+    local link = '_this group is fucked_'
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+    api.sendReply(msg,link,true)
+end
+
+
+
+
+
+return {
+  action = action,
+  triggers = triggers
+}
